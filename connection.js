47ff7e6b6daabbf0b43cde5625186abe059594ff
valie <isauravpr@gmail.com>
2024-05-01 22:57:39 -0600

    [Fix] connection

--- connection.js
+++ connection.js
@@ -1,7 +1,30 @@
 import mongoose from "mongoose";
+import { config } from "dotenv";
 
+config();
+
+const MONGODB_URI = process.env.MONGODB_URI;
 
 mongoose
-  .connect("mongodb://localhost/ecommerce", {})
-  .then((db) => console.log("db is connected"))
-  .catch((error) => console.log(error + "db is NOT connected"));
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  })
+  .then((db) => console.log("Connected to database"))
+  .catch((err) => {
+    console.error("Error connecting to database:", err);
+    process.exit(1); 
+  });
+
+
+mongoose.connection.on("disconnected", () => {
+  console.log("MongoDB disconnected!");
+  mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  });
+});
